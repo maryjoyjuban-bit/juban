c5dc350fb89d5cee3f01abe192670babfc48cacc
maryjoyjuban-bit <maryjoyjuban@gmail.com>
2026-01-29 22:46:37 +0800

    Update even-odd-checker.cpp

--- even-odd-checker.cpp
+++ even-odd-checker.cpp
@@ -6,8 +6,8 @@ int main() {
 cout << "Enter a number: ";
 cin >> num;
 
-if (num % 2=0) {
- cout << num << " is even. " << endl;
+if (num % 2==0) {
+ cout << num << " is even. " << endl; 
 } else {
  cout << num << "is odd. " << endl;
 }
